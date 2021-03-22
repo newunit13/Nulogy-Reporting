01f2da2a4e4d0a3e2b3d455b6ed8ea5330fd139d
Edward Russell <newunit13@gmail.com>
2021-03-22 15:00:59 -0400

    added encoding/decoding

--- credentials.py
+++ credentials.py
@@ -2,4 +2,4 @@ import base64
 
 username = ''
 password = ''
-secret_key = base64.b64encode(f"{username}:{password}")
+secret_key = base64.b64encode(f"{username}:{password}".encode('utf-8')).decode('utf-8')
