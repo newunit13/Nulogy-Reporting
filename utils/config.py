e014c221b5bab4f07b758c5d207853723b6d3dec
Edward Russell <newunit13@gmail.com>
2021-08-18 17:04:51 -0400

    changed stored keys

--- utils/config.py
+++ utils/config.py
@@ -9,6 +9,8 @@ _KVUri = f"https://{_keyVaultName}.vault.azure.net"
 
 _client = SecretClient(vault_url=_KVUri, credential=_credential)
 
-NULOGY_SECRET_KEY   = _client.get_secret("NulogySecretKey").value
-AZURE_DB_UN         = _client.get_secret("AzureDatabaseUsername").value
-AZURE_DB_PW         = _client.get_secret("AzureDatabasePassword").value
+NULOGY_SECRET_KEY           = _client.get_secret("NulogySecretKey").value
+AZURE_DB_CONNECTION_STRING  = _client.get_secret("AzureDatabaseConnectionString").value
+DAX_DB_CONNECTION_STRING    = _client.get_secret("DAX-DB-ConnectionString").value
+SMTP_USERNAME               = _client.get_secret("SMTP-Username").value
+SMTP_PASSWORD               = _client.get_secret("SMTP-Password").value
