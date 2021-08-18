347056e9f035c33a565b529355a7c8ed7e172a9a
Edward Russell <newunit13@gmail.com>
2021-08-18 17:03:41 -0400

    moved conn str to config & added remote db

--- utils/sql.py
+++ utils/sql.py
@@ -1,15 +1,11 @@
 import pyodbc 
 from typing import List
+from utils.config import DAX_DB_CONNECTION_STRING, AZURE_DB_CONNECTION_STRING
 
 
-def query(query: str) -> List[tuple]:
-    conn_str = (
-        r'Driver={ODBC Driver 17 for SQL Server};'
-        r'Server=B4BSQL.accutec.ad;'
-        r'Database=DynamicsAX_Prod;'
-        r'Trusted_Connection=yes;'
-        )
-    cnxn = pyodbc.connect(conn_str)
+def query(query: str, azure: bool=False) -> List[tuple]:
+
+    cnxn = pyodbc.connect(DAX_DB_CONNECTION_STRING) if not azure else pyodbc.connect(AZURE_DB_CONNECTION_STRING)
     cursor = cnxn.cursor()
 
     cursor.execute(query)
