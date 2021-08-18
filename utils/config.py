2d81b4c18683a51e04c6c54b8dd940f5a359c05f
Edward Russell <newunit13@gmail.com>
2021-08-18 16:18:22 -0400

    Moved credentials to Azure keyvault

--- utils/config.py
+++ utils/config.py
@@ -4,9 +4,11 @@ from azure.keyvault.secrets import SecretClient
 
 _credential = DefaultAzureCredential()
 
-keyVaultName = os.environ["AZURE_VAULT_NAME"]
-KVUri = f"https://{keyVaultName}.vault.azure.net"
+_keyVaultName = os.environ["AZURE_VAULT_NAME"]
+_KVUri = f"https://{_keyVaultName}.vault.azure.net"
 
-client = SecretClient(vault_url=KVUri, credential=_credential)
+_client = SecretClient(vault_url=_KVUri, credential=_credential)
 
-print(client.get_secret("NulogyUsername").value)
+NULOGY_SECRET_KEY   = _client.get_secret("NulogySecretKey").value
+AZURE_DB_UN         = _client.get_secret("AzureDatabaseUsername").value
+AZURE_DB_PW         = _client.get_secret("AzureDatabasePassword").value
