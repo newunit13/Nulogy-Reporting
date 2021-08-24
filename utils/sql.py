5f4f42d7a5f526db7481a733378ecc090db50957
Edward Russell <newunit13@gmail.com>
2021-08-24 16:47:00 -0400

    added insert and update

--- utils/sql.py
+++ utils/sql.py
@@ -14,3 +14,29 @@ def query(query: str, azure: bool=False) -> List[tuple]:
     cnxn.close()
 
     return result
+
+def insert(table: str, record: str, azure: bool=False) -> None:
+
+    cnxn = pyodbc.connect(DAX_DB_CONNECTION_STRING) if not azure else pyodbc.connect(AZURE_DB_CONNECTION_STRING)
+    cursor = cnxn.cursor()
+
+    cursor.execute(f"INSERT INTO [{table}] VALUES ({record})")
+    cursor.commit()
+
+    cnxn.close()
+
+def update(table: str, key_column: str, key_value: str, record: str, azure: bool=False) -> None:
+
+    cnxn = pyodbc.connect(DAX_DB_CONNECTION_STRING) if not azure else pyodbc.connect(AZURE_DB_CONNECTION_STRING)
+    cursor = cnxn.cursor()
+
+    statement = f"""
+UPDATE {table}
+SET {', '.join([f"[{column}] = '{value}'" for column, value in record])}
+WHERE [{key_column}] = '{key_value}'
+    """
+
+    cursor.execute(statement)
+    cursor.commit()
+
+    cnxn.close()
