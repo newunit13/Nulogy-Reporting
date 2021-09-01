1673a9ecfbe61247e0caace09c4f3378192986ad
Edward Russell <newunit13@gmail.com>
2021-09-01 15:30:46 -0400

    Moved UOM lookup structure to NU module

--- utils/nulogy.py
+++ utils/nulogy.py
@@ -1,11 +1,13 @@
 from utils.config import NULOGY_SECRET_KEY
-from typing import List
+from typing import List, Dict
 from time import sleep
 from datetime import datetime
+from random import randint
 import requests
 import json
 import csv
 
+uoms = None
 
 def downlad_report(download_url: str) -> str:
 
@@ -64,10 +66,12 @@ def get_report(report_code: str, columns: List[str], filters: List[dict]=[], sor
         if response.status_code != 201:
             with open(F'ERROR-{report_code}-{datetime.now().strftime("%Y%m%d-%H%M")}.txt', 'w') as outfile:
                 outfile.write(f'{response.status_code}-{response.text}')
-            if error_count > 3:
-                raise Exception
-            error_count += 1
-            sleep(10)
+                outfile.write(f'Error count: {error_count}')
+                if error_count > 3:
+                    outfile.write(f'Too many failed attempts. Exiting')
+                    raise Exception
+                error_count += 1
+            sleep(randint(60,120))
 
     # small sleep to give the report a chance to generate before polling for a download link
     sleep(15)
@@ -88,6 +92,100 @@ def get_report(report_code: str, columns: List[str], filters: List[dict]=[], sor
         next(report)
 
     return report
+
+def get_uom_list() -> Dict:
+    report_code = "uom_ratios"
+    columns = ["code", "unit_of_measure", "ratio", "conversion_unit_of_measure"]
+
+    report = get_report(report_code=report_code, columns=columns, headers=False)
+
+    uoms = dict()
+    for line in report:
+        item_code   = line[0]
+        from_unit   = line[1]
+        factor      = float(line[2])
+        to_unit     = line[3]
+
+        if item_code not in uoms:
+            uoms[item_code] = {
+                "conversions" : [{"from_unit": from_unit, "factor": factor, "to_unit": to_unit}]
+            }
+        else:
+            uoms[item_code]["conversions"].append({"from_unit": from_unit, "factor": factor, "to_unit": to_unit})
+
+    report_code = "item_master"
+    columns = ["code", "base_unit_of_measure"]
+    filters = [{"columns": "inactive", "operator": "=", "threshold": "False"}]
+
+    report = get_report(report_code=report_code, columns=columns, headers=False)
+
+    for line in report:
+        item_code = line[0]
+        base_unit = line[1]
+
+        if item_code in uoms:
+            uoms[item_code].update({"base_unit": base_unit})
+        else:
+            uoms[item_code] = {
+                "base_unit"     : base_unit,
+                "conversions"   : [{"from_unit": base_unit, "factor": 1, "to_unit": base_unit}]
+            }
+    return uoms
+
+def convertToBaseUnits(item_code: str, unit_of_measure: str, number_of_units: float) -> float:
+    '''
+        Returns the number of base units for a specified item.
+
+            Parameter:
+                item_code (str)         : The Nulogy Item Code
+                unit_of_measure (str)   : The unit of measure to be converted from
+                number_of_units (float) : The number of units to be converted into base units
+
+            Returns:
+                number_of_units (float) : The number of base units 
+    '''
+
+    short_to_long_uom = {
+        'ea'    : 'eaches',
+        'cs'    : 'cases',
+        'pl'    : 'pallets',
+        'rl'    : 'rolls',
+        'lbs'   : 'pounds',
+        'bdl'   : 'bundles',
+        'pk'    : 'packs',
+        'kg'    : 'kilograms',
+        'ltr'   : 'liters',
+        'box'   : 'boxes',
+        'gal'   : 'gallons',
+        'ft'    : 'feet'
+    }
+    
+    global uoms
+    if uoms == None:
+        uoms = get_uom_list()
+
+    # if a short code UOM is supplied convert it over to the long form
+    if unit_of_measure in short_to_long_uom:
+        unit_of_measure = short_to_long_uom[unit_of_measure]
+
+    # if the number of units wasn't in a numeric type convert it
+    if not isinstance(number_of_units, float):
+        number_of_units = float(number_of_units)
+
+    item_base_unit = uoms[item_code]["base_unit"]
+    if unit_of_measure == item_base_unit:
+        return number_of_units
+
+    for conversion in uoms[item_code]["conversions"]:
+        if conversion["from_unit"] == unit_of_measure:
+            number_of_units = number_of_units * conversion["factor"]
+            unit_of_measure = conversion["to_unit"]
+            break
+    else:
+        raise Exception(f"Unable to find {unit_of_measure} in {item_code} conversions.")
+
+    return convertToBaseUnits(item_code, unit_of_measure, number_of_units)
+
 
 if __name__ == '__main__':
 
