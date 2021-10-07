e6040c3878bb994016d09d28bb9ee21491d54183
Edward Russell <newunit13@gmail.com>
2021-10-07 12:05:10 -0400

    added UOMs

--- nulogy_item_cost_update.py
+++ nulogy_item_cost_update.py
@@ -3,7 +3,7 @@ import utils.sql as sql
 import csv
 
 report_code = "item_master"
-columns = ["code", "cost_per_unit", "item_type", "uuid"]
+columns = ["code", "cost_per_unit", "default_unit_of_measure", "uuid"]
 filters = [{"column": "item_type", "operator": "starts with", "threshold": "A"},
            {"column": "inactive", "operator": "=", "threshold": "false"}]
 
@@ -12,19 +12,25 @@ report = nu.get_report(report_code=report_code, columns=columns, filters=filters
 
 nulogy_items = dict()
 for row in report:
-    nulogy_items[row[0]] = {'nulogy_cost': row[1], "UUID": row[3]}
+    nulogy_items[row[0]] = {'nulogy_cost': row[1], "nulogy_uom": row[2], "UUID": row[3]}
 
 
 query = """
 SELECT
 	ivt.NAMEALIAS				[ItemID]
-	,ivtp.PRICE/CASE 
-		WHEN ivtp.PRICEUNIT = 0 
+	,ivti.PRICE/CASE 
+		WHEN ivti.PRICEUNIT = 0 
 		THEN 1 
-		ELSE ivtp.PRICEUNIT END	[Cost]
+		ELSE ivti.PRICEUNIT END	[Cost]
     ,ivt.ITEMID                 [DAX ItemID]
+	,ivti.UNITID				[UOM]
 FROM INVENTTABLE ivt
-LEFT JOIN INVENTTABLEMODULE ivtp ON ivtp.ITEMID = ivt.ITEMID and ivtp.DATAAREAID = ivt.DATAAREAID and ivtp.MODULETYPE = 0
+
+LEFT JOIN INVENTTABLEMODULE ivti ON 
+	ivti.ITEMID     = ivt.ITEMID and 
+	ivti.DATAAREAID = ivt.DATAAREAID and 
+	ivti.MODULETYPE = 1
+
 WHERE ivt.DATAAREAID = 'act'
   and ivt.ITEMGROUPID = 'AI'
   and ivt.NAMEALIAS <> ''
@@ -32,14 +38,15 @@ WHERE ivt.DATAAREAID = 'act'
 
 r = sql.query(query=query)
 
-for item, cost, dax_id in r:
+for item, cost, dax_id, dax_uom in r:
     item = item.strip()
     if item in nulogy_items:
         nulogy_items[item]["dax_cost"] = f"{round(cost, 5):.5f}"
+        nulogy_items[item]["dax_uom"] = f"{dax_uom}"
 
 with open('output/item_cost_update.csv', 'w', newline='') as csvfile:
 
-    fieldnames = ['Code', 'Old Cost Per Unit', 'Cost Per Unit', 'UUID']
+    fieldnames = ['Code', 'Old Cost Per Unit', 'Cost Per Unit', 'Nulogy UoM', 'DAX UoM', 'UUID']
     csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     csv_writer.writeheader()
@@ -49,5 +56,6 @@ with open('output/item_cost_update.csv', 'w', newline='') as csvfile:
             csv_writer.writerow({"Code"             : item, 
                                  "Old Cost Per Unit": record.get("nulogy_cost"), 
                                  "Cost Per Unit"    : record.get("dax_cost"), 
+                                 "Nulogy UoM"       : record.get("nulogy_uom"),
+                                 "DAX UoM"          : record.get("dax_uom"),
                                  "UUID"             : record.get("UUID")})
-
